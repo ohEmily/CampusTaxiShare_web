243214192b982fee341c89afc78cae0e1e8ae883
Emily Pakulski <enp2111@columbia.edu>
2014-06-12 13:09:58 -0700

    Some cleanup before getting started again

--- app.js
+++ app.js
@@ -1,41 +1,17 @@
-/**
- * Module dependencies
- */
+var port = 3000;
 
+/** Set up Express with module dependencies */
 var express = require('express'),
-	api = require('./routes/api'),
-	http = require('http'),
-	path = require('path');
-
+	http = require('http');
+	
 var app = module.exports = express();
 
 
-/**
- * Configuration
- */
-
+/** Configuration */
 // all environments
-app.set('port', process.env.PORT || 3000);
-app.use(express.logger('dev'));
-app.use(express.bodyParser()); 	
-app.use(express.methodOverride()); 	
-//app.use(app.router); – not sure if we need this
+app.set('port', process.env.PORT || port);
 
-// development only
-if (app.get('env') === 'development') {
-	app.use(express.errorHandler());
-}
-
-// production only
-if (app.get('env') === 'production') {
-	// TODO
-};
-
-
-/**
- * Routes
- */
-
+/** Routes */
 // serve all asset files from necessary directories
 app.use("/js", express.static(__dirname + "/public/js"));
 app.use("/css", express.static(__dirname + "/public/css"));
@@ -43,19 +19,13 @@ app.use("/partials", express.static(__dirname + "/public/partials"));
 app.use("/lib", express.static(__dirname + "/public/lib"));
 app.use("/fonts", express.static(__dirname + "/public/fonts"));
 app.use("/img", express.static(__dirname + "/public/img"));
-// JSON API
-app.get('/api/name', api.name);
 
 // redirect all others to the index (HTML5 history)
 app.all("/*", function(req, res, next) {
 	res.sendfile("index.html", { root: __dirname + "/public" });
 });
 
-
-/**
- * Start Server
- */
-
+/** Start Server */
 http.createServer(app).listen(app.get('port'), function () {
 	console.log('Express server listening on port ' + app.get('port'));
 });
