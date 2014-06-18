bd09a1db41298c68283ad954a3e47ca3a2b2a104
Emily Pakulski <enp2111@columbia.edu>
2014-06-18 15:01:42 -0700

    Sort of posting created groups to Parse, but Parse relations/pointers are giving me trouble.

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -84,7 +84,8 @@ angular.module('myApp.controllers', []).
 					// Do something with the returned Parse.Object values
 					for (var i = 0; i < results.length; i++) { 
 						$(".locationPick").append(new 
-							Option(results[i].get("locationName"), "Option" + i));
+							Option(results[i].get("locationName"), 
+							results[i].get("locationId")));
 					}
 				},
 				error: function(error) {
@@ -96,9 +97,44 @@ angular.module('myApp.controllers', []).
 		})();
 		
 		
-		$scope.createGroup = function()
+		$scope.submitGroup = function()
 		{
+			/** Returns the Location object with the name passed as needle. */
+			var getLocationName = function(needle) 
+			{
+				// https://parse.com/docs/js_guide#objects-retrieving
+				var Location = Parse.Object.extend("Location");
+				var query = new Parse.Query(Location);
+				query.equalTo("locationId", needle);
+				query.find(
+				{
+					success: function(found_object) {
+						return found_object.id;
+					},
+					error: function(error) {
+						alert("Error: " + error.code + " " + error.message);
+						return null;
+					}
+				});
+			};
 			
+			// https://parse.com/docs/js_guide#objects-saving
+			var Group = Parse.Object.extend("Group");
+			var newGroup = new Group();
+			
+			newGroup.set("owner", Parse.User.current());
+			newGroup.set("departure_time_date", $scope.group.when);
+			newGroup.set("start_point", getLocationName($scope.group.start));
+			newGroup.set("end_point", getLocationName($scope.group.end));
+			
+			newGroup.save(null, {
+				success: function(newGroup) {
+					alert('New object created with ObjectId: ' + newGroup.id);
+				},
+				error: function(newGroup, error) {
+					alert('this failed');
+				}
+			});
 		}
 		
 		$scope.logout = function()
